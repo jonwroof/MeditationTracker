9af379b6ff3ff0f980864020896d143b2383720b
Fawhn Hanlon <whanlon@ggc.edu>
2021-03-03 03:45:48 -0500

    Adds Timer to Tab 2

--- meditation-tracker/src/app/tab2/tab2.page.ts
+++ meditation-tracker/src/app/tab2/tab2.page.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+const circleR = 80;
+const circleDasharray = 2 * Math.PI * circleR;
 
 @Component({
   selector: 'app-tab2',
@@ -6,7 +10,65 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  time: BehaviorSubject<string> = new BehaviorSubject('00:00');
+  percent: BehaviorSubject<number> = new BehaviorSubject(100);
+
+  timer: number; //in seconds
+
+  interval;
+
+  startDuration = 1;
+
+  circleR = circleR;
+  circleDasharray = circleDasharray;
+  state: 'start' | 'stop' = 'stop';
 
   constructor() {}
+
+  click(){
+    alert('Meditation Session Started')
+  }
+
+  startTimer(duration: number){
+    this.state = 'start';
+    clearInterval(this.interval);
+    this.timer = duration * 60;
+    this.updateTimeValue();
+    this.interval = setInterval( () => {
+      this.updateTimeValue();
+    }, 1000);
+  }
+
+  stopTimer(){
+    clearInterval(this.interval);
+    this.time.next('00:00');
+    this.state = 'stop';
+  }
+
+
+  updateTimeValue(){
+    let minutes: any = this.timer / 60;
+    let seconds: any = this.timer % 60;
+
+    minutes = String ('0' + Math.floor(minutes)).slice(-2);
+    seconds = String('0' + Math.floor(seconds)).slice(-2);
+
+    const text = minutes + ':' + seconds;
+    this.time.next(text);
+
+    const totalTime = this.startDuration * 60;
+    const percentage = ((totalTime - this.timer) / totalTime) * 100;
+    this.percent.next(percentage);
+
+    --this.timer;
+    if (this.timer < -1){
+      this.startTimer(this.startDuration);
+    }
+  }
+
+  percentageOffset(percent){
+    const percentFloat = percent / 100;
+    return circleDasharray * (1 - percentFloat);
+  }
 
 }
