4cae81004d2dd865ac8c22ff40ad5713829e56bb
Jonathan Roof <jwroofsound@gmail.com>
2021-03-08 18:35:32 -0500

    karma config change

--- karma.conf.js
+++ karma.conf.js
@@ -39,7 +39,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['PhantomJS'],
-    singleRun: false,
+    singleRun: true,
     restartOnFileChange: true
   });
 };
